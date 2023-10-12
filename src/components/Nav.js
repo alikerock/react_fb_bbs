25b5eee277b836b5f2a1c444e6b514bb02a10dd7
rock's ezweb <alikerock@gmail.com>
2023-10-12 10:22:50 +0900

    nav create

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+/*
+function Nav(){
+  return(
+    <nav>Navigation</nav>
+  ) 
+}
+const Nav = ()=>{
+  return(
+    <nav>Navigation</nav>
+  )
+}
+*/
+const Nav = ()=><nav>Navigation</nav>;  
+
+export default Nav;
