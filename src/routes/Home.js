5ae09568db6d85ea748497c11fe5cb6c8d760724
rock's ezweb <alikerock@gmail.com>
2023-10-12 12:10:36 +0900

    [add]포스트 출력

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,10 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import { db } from '../firebase';
-import { collection, addDoc, serverTimestamp } from "firebase/firestore"; 
+import { collection, getDocs, addDoc, serverTimestamp } from "firebase/firestore"; 
 
 const Home = ()=>{
   const [post,setPost] = useState('');
+  const [posts,setPosts] = useState([]);
   
   const onChange = (e)=>{
     //const val = e.target.value; //ECMA Script 2012
@@ -12,19 +13,43 @@ const Home = ()=>{
   }
   const onSubmit = async (e) =>{
     e.preventDefault();
-    const docRef = await addDoc(collection(db, "posts"), {
-      content: post,
-      date: serverTimestamp()
-    });
-    console.log("Document written with ID: ", docRef.id);
+
+    try{
+        const docRef = await addDoc(collection(db, "posts"), {
+          content: post,
+          date: serverTimestamp()
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch(e){
+      console.log(e);
+    }
   }
-  console.log(post);
+  const getPosts = async () =>{
+    const querySnapshot = await getDocs(collection(db, "posts"));
+    querySnapshot.forEach((doc) => {
+      const postObj = {
+        ...doc.data(),
+        id:doc.id
+      }      
+      setPosts((prev)=>[postObj,...prev]);
+    });
+  }
+
+  useEffect(()=>{
+    getPosts();
+  },[])
+
   return(
     <div>
       <form onSubmit={onSubmit}>
         <input type="text" name="post" value={post} placeholder='포스트 쓰기' onChange={onChange}></input>
         <input type="submit" value="입력"></input>
       </form>
+      <ul>
+      {
+        posts.map(item=><li key={item.id}>{item.content}</li>)
+      }
+      </ul>
     </div>
   )
 }
