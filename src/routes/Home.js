3dc026486f1ffee3bf6d2e33f7a20cdc38a1b556
rock's ezweb <alikerock@gmail.com>
2023-10-12 11:42:42 +0900

    [add]post 입력

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,28 @@
 import React,{useState} from 'react';
-import { collection, addDoc } from "firebase/firestore"; 
+import { db } from '../firebase';
+import { collection, addDoc, serverTimestamp } from "firebase/firestore"; 
 
 const Home = ()=>{
   const [post,setPost] = useState('');
+  
   const onChange = (e)=>{
-    //const value = e.target.value; //ECMA Script 2012
+    //const val = e.target.value; //ECMA Script 2012
     const {target:{value}} = e; //ES6
     setPost(value);
   }
-  const onSubmit = (e) =>{
+  const onSubmit = async (e) =>{
     e.preventDefault();
-
+    const docRef = await addDoc(collection(db, "posts"), {
+      content: post,
+      date: serverTimestamp()
+    });
+    console.log("Document written with ID: ", docRef.id);
   }
+  console.log(post);
   return(
     <div>
       <form onSubmit={onSubmit}>
-        <input type="text" value={post} placeholder='포스트 쓰기' onChange={onChange}></input>
+        <input type="text" name="post" value={post} placeholder='포스트 쓰기' onChange={onChange}></input>
         <input type="submit" value="입력"></input>
       </form>
     </div>
