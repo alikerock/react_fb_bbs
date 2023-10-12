c4754a9149e4bb1ee0607f943db4b2369d5a27f3
rock's ezweb <alikerock@gmail.com>
2023-10-12 11:20:52 +0900

    [add]입력폼

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { collection, addDoc } from "firebase/firestore"; 
 
-const Home = ()=><h2>Home Page</h2>
+const Home = ()=>{
+  const [post,setPost] = useState('');
+  const onChange = (e)=>{
+    //const value = e.target.value; //ECMA Script 2012
+    const {target:{value}} = e; //ES6
+    setPost(value);
+  }
+  const onSubmit = (e) =>{
+    e.preventDefault();
+
+  }
+  return(
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={post} placeholder='포스트 쓰기' onChange={onChange}></input>
+        <input type="submit" value="입력"></input>
+      </form>
+    </div>
+  )
+}
 
 export default Home;
