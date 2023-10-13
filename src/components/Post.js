e51463cc79305fcddc6e7f575c66a4eae465d5ff
rock's ezweb <alikerock@gmail.com>
2023-10-13 09:59:02 +0900

    [add]delete post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,19 +1,28 @@
 import React from "react";
+import { db } from "../firebase";
+import { doc, deleteDoc } from "firebase/firestore";
 
-const Post = ({postObj,userConfirm})=>(
- 
+const Post = ({postObj,userConfirm})=>{
+  console.log(postObj);
+  const deletePost = async () =>{
+    if(window.confirm('정말 삭제할까요')){
+      await deleteDoc(doc(db, "posts", postObj.id));
+    }    
+  }
+  
+  return( 
   <li>
     <h4>{postObj.content}</h4>
     { 
       userConfirm && (
         <>
-          <button>Delete</button>  
+          <button onClick={deletePost}>Delete</button>  
           <button>Edit</button>  
         </>
       )
     }
   </li>
-
-);
+  )
+};
 
 export default Post;
